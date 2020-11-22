81af12bc3b00aaa40555f05d55ce6fab72244668
RyanMKrol <ryankrol.m@gmail.com>
2020-11-22 17:38:04 +0000

    Add method to fetch constituent links
    This method will fetch a list of links for each stock
    in the index we pass through to main()

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-process.stdout.write('Hello World!');
+import curl from 'curl';
+import cheerio from 'cheerio';
+
+/**
+ * Main
+ *
+ * @param {string} index Index to run screener for
+ */
+async function main(index) {
+  process.stdout.write('Doing a main!\n');
+
+  const constituentsConfig = {
+    ftse100: 'https://www.lse.co.uk/share-prices/indices/ftse-100/constituents.html',
+    ftse250: 'https://www.lse.co.uk/share-prices/indices/ftse-250/constituents.html',
+    aim100: 'https://www.lse.co.uk/share-prices/indices/ftse-aim-100/constituents.html',
+  };
+
+  const constituentsUrl = constituentsConfig[index];
+
+  process.stdout.write('about to fetch the stuff\n');
+
+  return new Promise((resolve, reject) => {
+    curl.get(constituentsUrl, (err, response, body) => {
+      try {
+        const $ = cheerio.load(body);
+
+        const tickers = $('.sp-constituents tr > td:nth-child(1) a:nth-child(2)')
+          .map((i, elem) => $(elem).attr('href'))
+          .get();
+
+        resolve(tickers);
+      } catch (error) {
+        reject(new Error('Failed to fetch the constituents'));
+      }
+    });
+  });
+}
+
+main('ftse100');

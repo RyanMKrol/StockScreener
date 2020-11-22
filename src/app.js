c598b41ad939c4482ff9e6e569b538e9b7412ddd
RyanMKrol <ryankrol.m@gmail.com>
2020-11-22 17:51:29 +0000

    Replace stock price links with fundamentals links

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ import cheerio from 'cheerio';
  * @param {string} index Index to run screener for
  */
 async function main(index) {
-  process.stdout.write('Doing a main!\n');
-
   const constituentsConfig = {
     ftse100: 'https://www.lse.co.uk/share-prices/indices/ftse-100/constituents.html',
     ftse250: 'https://www.lse.co.uk/share-prices/indices/ftse-250/constituents.html',
@@ -17,18 +15,18 @@ async function main(index) {
 
   const constituentsUrl = constituentsConfig[index];
 
-  process.stdout.write('about to fetch the stuff\n');
-
   return new Promise((resolve, reject) => {
     curl.get(constituentsUrl, (err, response, body) => {
       try {
         const $ = cheerio.load(body);
 
-        const tickers = $('.sp-constituents tr > td:nth-child(1) a:nth-child(2)')
+        const baseLinks = $('.sp-constituents tr > td:nth-child(1) a:nth-child(2)')
           .map((i, elem) => $(elem).attr('href'))
           .get();
 
-        resolve(tickers);
+        const links = baseLinks.map((x) => x.replace('SharePrice.asp', 'share-fundamentals.asp'));
+
+        resolve(links);
       } catch (error) {
         reject(new Error('Failed to fetch the constituents'));
       }

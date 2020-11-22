cd79edfa969f17e38a44a0bd51fb856ce46ab99f
RyanMKrol <ryankrol.m@gmail.com>
2020-11-22 18:34:47 +0000

    Add functionality to fetch revenue data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import curl from 'curl';
 import cheerio from 'cheerio';
+import async from 'async';
+
+const SIMULTANEOUS_FUNDAMENTALS_FETCHES = 5;
 
 /**
  * Main
@@ -7,6 +10,47 @@ import cheerio from 'cheerio';
  * @param {string} index Index to run screener for
  */
 async function main(index) {
+  const fundamentalsLinks = await fetchFundamentalsLinks(index);
+
+  const data = await async.mapLimit(
+    fundamentalsLinks,
+    SIMULTANEOUS_FUNDAMENTALS_FETCHES,
+    async (link) => new Promise((resolve, reject) => {
+      curl.get(link, (err, response, body) => {
+        try {
+          const $ = cheerio.load(body);
+
+          const baseLinks = $('.sp-fundamentals__table tr').filter(
+            (i, elem) => $(elem)
+              .children('td')
+              .first()
+              .text() === 'Revenue',
+          );
+
+          const revenueData = $(baseLinks)
+            .children('td')
+            .map((i, elem) => (i === 0 ? undefined : $(elem).text()))
+            .get()
+            .map((x) => parseInt(x.replace(',', ''), 10));
+
+          resolve(revenueData);
+        } catch (error) {
+          reject(new Error('Failed to fetch the fundamentals'));
+        }
+      });
+    }),
+  );
+
+  console.log(data);
+}
+
+/**
+ * Method to fetch the fundamentals links for each stock in a given index
+ *
+ * @param {string} index Index to run screener for
+ * @returns {Array.<string>} Array of links correlating to the fundamentals page for each stock
+ */
+async function fetchFundamentalsLinks(index) {
   const constituentsConfig = {
     ftse100: 'https://www.lse.co.uk/share-prices/indices/ftse-100/constituents.html',
     ftse250: 'https://www.lse.co.uk/share-prices/indices/ftse-250/constituents.html',

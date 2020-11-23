4dde93e07da5b64ca7f99ecbc19f5ed7308bab49
RyanMKrol <ryankrol.m@gmail.com>
2020-11-23 21:59:03 +0000

    Add filter for performance against first X years

--- src/modules/filters/Filters.js
+++ src/modules/filters/Filters.js
@@ -41,6 +41,32 @@ function increasingForLastNumPointsFilter(array, attribute, threshold, pointsNee
 
 /**
  * Filter to find companies where an attribute is above a
+ * certain threshold for the first X years running
+ *
+ * @param {Array.<module:app.Fundamentals>} array Array of fundamentals
+ * @param {string} attribute The attribute we want to filter on
+ * @param {number} threshold The threshold we want to enforce on the fundamentals metric
+ * @param {number} pointsNeeded The number of years of data needed
+ * @returns {Array.<module:app.Fundamentals>} The remaining fundamentals that fit this filter
+ */
+function increasingForFirstNumPointsFilter(array, attribute, threshold, pointsNeeded) {
+  return array.filter((stockFundamentals) => {
+    // get the differences in each year
+    const runningDifferences = runningPercentageDiff(stockFundamentals[attribute]);
+
+    // reduce this array by the number of years I care about
+    const reducedRunningDifferences = runningDifferences.slice(0, pointsNeeded);
+
+    // if there are fewer years than I've specified, remove the item
+    if (reducedRunningDifferences.length !== pointsNeeded) return 0;
+
+    // if every year is above threshold, keep it in the group
+    return reducedRunningDifferences.every((x) => x >= threshold);
+  });
+}
+
+/**
+ * Filter to find companies where an attribute is above a
  * certain threshold for every year
  *
  * @param {Array.<module:app.Fundamentals>} array Array of fundamentals
@@ -58,4 +84,9 @@ function increasingForAllPointsFilter(array, attribute, threshold) {
   });
 }
 
-export { startVsEndDiffFilter, increasingForLastNumPointsFilter, increasingForAllPointsFilter };
+export {
+  startVsEndDiffFilter,
+  increasingForFirstNumPointsFilter,
+  increasingForLastNumPointsFilter,
+  increasingForAllPointsFilter,
+};
